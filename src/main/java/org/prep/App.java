6862824c8f0d7a648028b2f319eedbdebe54f69e
Ravi Raizada <raviraizadagm@gmail.com>
2019-11-08 00:30:44 +0530

    Adding project

---
+++ src/main/java/org/prep/App.java
@@ -0,0 +1,13 @@
+package org.prep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
